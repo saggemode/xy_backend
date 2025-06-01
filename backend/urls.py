643e4d11180ec793d2177b8e4056e874f531b84f
saggemode <saggemode@gmail.com>
2025-06-02 00:57:18 +0100

    initial commit4

--- backend/urls.py
+++ backend/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
   path('auth/', include('djoser.urls.authtoken')),
-path('api/product', include('core.urls')),
+path('api/products/', include('core.urls')),
 
 ]
 
