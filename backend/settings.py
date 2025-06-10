a9e12124c48d9bc94f634c12e0e3c2cd13597d43
saggemode <saggemode@gmail.com>
2025-06-11 00:30:44 +0100

    initial commit4

--- backend/settings.py
+++ backend/settings.py
@@ -207,11 +207,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+       
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    
 }
 
 from datetime import timedelta
