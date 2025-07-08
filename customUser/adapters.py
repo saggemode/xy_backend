63cd0a7892919b810609783ca46babb8ec636bf2
saggemode <saggemode@gmail.com>
2025-07-08 19:52:22 +0100

    initial commit5

--- customUser/adapters.py
+++ customUser/adapters.py
@@ -1,11 +1,10 @@
-# accounts/adapters.py
 from allauth.account.adapter import DefaultAccountAdapter
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=True):
         user = super().save_user(request, user, form, commit=False)
-        user.full_name = request.data.get('full_name')
-        user.phone = request.data.get('phone')
+        user.full_name = form.cleaned_data.get('full_name')
+        user.phone = form.cleaned_data.get('phone')
         if commit:
             user.save()
         return user
--- customUser/serializers.py
+++ customUser/serializers.py
@@ -1,8 +1,5 @@
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class CustomRegisterSerializer(RegisterSerializer):
     full_name = serializers.CharField(required=True)
@@ -13,10 +10,3 @@ class CustomRegisterSerializer(RegisterSerializer):
         data['full_name'] = self.validated_data.get('full_name', '')
         data['phone'] = self.validated_data.get('phone', '')
         return data
-
-    def create(self, validated_data):
-        user = super().create(validated_data)
-        user.full_name = validated_data.get('full_name', '')
-        user.phone = validated_data.get('phone', '')
-        user.save()
-        return user
